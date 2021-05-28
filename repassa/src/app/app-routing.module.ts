7173ea64270a67c50159dd04d472d12c1ec4d06d
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-05-28 16:43:13 -0300

    Rotas

--- repassa/src/app/app-routing.module.ts
+++ repassa/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DenyLoginComponent } from './deny-login/deny-login.component';
+import { logging } from 'selenium-webdriver'; 
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+ {path: 'login', component: DenyLoginComponent},
+ {path: 'cadastro', component: CadastroComponent}
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
