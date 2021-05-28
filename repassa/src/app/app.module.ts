72c4b8e30567badcf0cb8d54dd550b2ed6189d96
unknown <maxdenyssonn@gmail.com>
2021-05-28 15:20:33 -0300

    task1: Login

--- repassa/src/app/app.module.ts
+++ repassa/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DenyLoginComponent } from './deny-login/deny-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DenyLoginComponent
   ],
   imports: [
     BrowserModule,
