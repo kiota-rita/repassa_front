bbf044ab9b9fd79e7766f15789ccdabcfe7d5607
Vinícius <vds199sp@gmail.com>
2021-05-28 15:31:44 -0300

    Desenvolvimento equipe home page

--- repassa/src/app/app.module.ts
+++ repassa/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquipeComponent } from './equipe/equipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipeComponent
   ],
   imports: [
     BrowserModule,
