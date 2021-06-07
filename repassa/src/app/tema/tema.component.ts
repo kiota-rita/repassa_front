815b0a11af843612f0a8a27066d082d78ca186f7
Washington Albuquerque <walbuque@gmail.com>
2021-06-07 12:24:30 -0300

    tema-get-post

--- repassa/src/app/tema/tema.component.ts
+++ repassa/src/app/tema/tema.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == ''){
+      alert('Sua sessão expirou, faça o login novamente.')
+      this.router.navigate(['/entrar'])
+    }
+
+    this.findAllTemas() 
+
   }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) =>{
+      this.listaTemas = resp
+    })
+
+  } 
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas() 
+      this.tema = new Tema()
+    })
+  }
+
+
 
 }
