1a6fa545e75c96a7c50b57cc045f77e7b1d2d58a
kiota-rita <rita-kiota@hotmail.com>
2021-06-08 18:45:25 -0300

    finalizando home

--- repassa/src/app/app-routing.module.ts
+++ repassa/src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { DenyLoginComponent } from './deny-login/deny-login.component';
 import { logging } from 'selenium-webdriver'; 
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
 
  {path: 'login', component: DenyLoginComponent},
  {path: 'cadastro', component: CadastroComponent},
- {path: 'inicio', component: InicioComponent}
+ {path: 'inicio', component: InicioComponent},
+
+ {path: 'home', component: HomeComponent}
 
 ];
 
